2d2d339f3edbe0fff0f4b5a52d77525ec0893287
latha-m-tz <latha.m@tamilzorous.com>
2025-10-27 11:51:09 +0530

    added user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable implements JWTSubject
     use HasFactory, Notifiable;
 
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
